8438e2b9dfa2ea3d7ecd7c7d5586fe664b8993b7
BRWCodeBR <allanalvess@hotmail.com>
2019-02-16 14:02:20 -0200

    Ajustes DB

--- User.Api/Controllers/UsersController.cs
+++ User.Api/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace User.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/user")]
     [ApiController]
     public class UsersController : ControllerBase
     {
