00b0496b7a8d6720e5b6fbf18f6a3db107ebc3e9
Rodrigo Belmonte <rod.b.oliveira@gmail.com>
2019-02-18 22:48:23 -0300

    Criado Contrato dos Users (problemas na hora de mandar pro Nuget)

---
+++ GeekBurger.Users.Contract/FoodRestriction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.Users.Contract
+{
+    public class FoodRestriction
+    {
+        public Guid FoodId { get; set; }
+        public string FoodName { get; set; }
+    }
+}
---
+++ GeekBurger.Users.Contract/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.Users.Contract
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public Guid FaceId { get; set; }
+
+        public List<FoodRestriction> FoodRestrictions { get; set; }
+    }
+}
